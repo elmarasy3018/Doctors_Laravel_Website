c664ededbc99b4ee1dae0d9e72f4269e545ab186
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2023-09-26 06:15:52 +0300

    Initial commit

---
+++ app/Http/Controllers/DoctorController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Doctor;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    public function index()
+    {
+        $doctors = Doctor::all();
+        // return $doctors;
+        return view('index', ['doctors' => $doctors]);
+    }
+
+    public function show(string $id)
+    {
+        $doctor = Doctor::findOrFail($id);
+        // return $doctor;
+        return view('show', ['doctor' => $doctor]);
+    }
+}
---
+++ database/factories/DoctorFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class DoctorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'specialty' => Str::random(10),
+            'description' => Str::random(10),
+        ];
+    }
+}
