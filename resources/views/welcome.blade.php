802eefc6eac619df33226a25380a67127709e212
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2023-09-28 00:57:59 +0300

    v4.7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,10 @@
             display: none;
             /* Safari and Chrome */
         }
+
+        .navbar {
+            transition: 600ms;
+        }
     </style>
 </head>
 
@@ -65,16 +69,16 @@
         </nav> --}}
     </header>
     <main>
-        <nav class="navbar border-b border-gray-200 sticky top-0 z-40">
+        <nav class="navbar sticky top-0 z-40">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <a href="" class="flex items-center">
+                    <img src="{{ asset('logos/2.png') }}" class="logo-1 h-16 mr-3" alt="Logo">
+                    <img src="{{ asset('logos/1.png') }}" class="logo-2 h-16 mr-3 hidden" alt="Logo">
                 </a>
                 <div class="flex md:order-2">
                     <button type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
-                        started</button>
+                        class="text-white hover:bg-sky-400 bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">Contact
+                        Us</button>
                     <button data-collapse-toggle="navbar-sticky" type="button"
                         class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         aria-controls="navbar-sticky" aria-expanded="false">
@@ -88,23 +92,25 @@
                 </div>
                 <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                     <ul
-                        class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        class="ul text-white flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
                         <li>
                             <a href="#"
-                                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                                aria-current="page">Home</a>
+                                class="li block py-2 pl-3 pr-4 rounded md:hover:bg-transparent md:hover:text-gray-300 md:p-0">Home</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                                class="li block py-2 pl-3 pr-4 rounded md:hover:bg-transparent md:hover:text-gray-300 md:p-0">About
+                                Dr. Samer</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                                class="li block py-2 pl-3 pr-4 rounded md:hover:bg-transparent md:hover:text-gray-300 md:p-0">Publications
+                                &amp; Scientific Activities</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                                class="li block py-2 pl-3 pr-4 rounded md:hover:bg-transparent md:hover:text-gray-300 md:p-0">National
+                                &amp; International Events</a>
                         </li>
                     </ul>
                 </div>
@@ -289,11 +295,22 @@
     </script>
     <script>
         const navbar = document.querySelector('.navbar');
+        const logo1 = document.querySelector('.logo-1');
+        const logo2 = document.querySelector('.logo-2');
+        const ul = document.querySelector('.ul');
         window.addEventListener('scroll', () => {
             if (window.scrollY >= 48) {
-                navbar.classList.add('bg-gray-400');
+                navbar.classList.add('bg-white');
+                logo1.classList.add('hidden');
+                logo2.classList.remove('hidden');
+                ul.classList.add('text-blue-800');
+                ul.classList.remove('text-white');
             } else {
-                navbar.classList.remove('bg-gray-400');
+                navbar.classList.remove('bg-white');
+                logo1.classList.remove('hidden');
+                logo2.classList.add('hidden');
+                ul.classList.add('text-white');
+                ul.classList.remove('text-blue-800');
             }
         })
     </script>
