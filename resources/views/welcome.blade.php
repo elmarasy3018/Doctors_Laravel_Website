d0e19b31d4b36d5c697d3697f3f55a69584e663f
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2023-09-27 23:49:58 +0300

    v4.6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
 
 <body class="bg-neutral-800">
     <header>
-        <nav class="bg-white border-gray-200">
+        <nav class="bg-white border-gray-200 sticky top-0">
             <div class="flex justify-around items-center p-3">
                 <div>
                     <i class="fa-solid fa-square-phone fa-lg"></i><span class="font-medium mx-2 mr-4">0122 883 3439 -
@@ -41,8 +41,75 @@
                 </div>
             </div>
         </nav>
+        {{-- <nav>
+            <div class="header sticky">
+                <div class="header-container">
+                    <div class="header-menu-btn">
+                        <i class="fa-solid fa-bars-staggered"></i>
+                    </div>
+                    <a href="index.html" class="header-logo">
+                        <img src="assets/img/header-logo.png" alt="Alex Neuro Spine - Dr. Samer Samy">
+                        <img class="blue-logo" src="assets/img/blue-header-logo.png"
+                            alt="Alex Neuro Spine - Dr. Samer Samy">
+                    </a>
+                    <div class="header-links">
+                        <a href="index.html">Home</a>
+                        <a href="about-us.html">About Dr. Samer</a>
+                        <a href="publications.html">Publications &amp; Scientific Activities</a>
+                        <a href="events.html">National &amp; International Events</a>
+                        <a href="news.html">News</a>
+                    </div>
+                    <a href="contact-us.html" class="header-btn"><i class="fa-solid fa-headset"></i> Contact us</a>
+                </div>
+            </div>
+        </nav> --}}
     </header>
     <main>
+        <nav class="navbar border-b border-gray-200 sticky top-0 z-40">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="https://flowbite.com/" class="flex items-center">
+                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo">
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                </a>
+                <div class="flex md:order-2">
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
+                        started</button>
+                    <button data-collapse-toggle="navbar-sticky" type="button"
+                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-sticky" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    <ul
+                        class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <div class="carousel">
             <div class="m-5">
                 <div id="default-carousel" class="relative w-full" data-carousel="static">
@@ -219,6 +286,16 @@
         prevButton.addEventListener("click", () => {
             slidesContainer.scrollLeft -= slideWidth;
         });
+    </script>
+    <script>
+        const navbar = document.querySelector('.navbar');
+        window.addEventListener('scroll', () => {
+            if (window.scrollY >= 48) {
+                navbar.classList.add('bg-gray-400');
+            } else {
+                navbar.classList.remove('bg-gray-400');
+            }
+        })
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
     <script src="https://kit.fontawesome.com/864870fe8f.js" crossorigin="anonymous"></script>
