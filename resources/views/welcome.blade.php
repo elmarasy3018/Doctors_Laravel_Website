2dff19068cfd7b3cec23a0334312d3700d016e88
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2023-09-27 21:25:09 +0300

    v4.5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,18 +45,18 @@
     <main>
         <div class="carousel">
             <div class="m-5">
-                <div id="default-carousel" class="relative w-full" data-carousel="slide">
-                    <!-- Carousel wrapper -->
+                <div id="default-carousel" class="relative w-full" data-carousel="static">
+                    {{-- <div id="default-carousel" class="relative w-full" data-carousel="slide"> --}}
                     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
                         @foreach ($doctors as $doctor)
                             <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
                                 <div class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                                    <div class="grid grid-cols-10 w-full h-56 md:h-96">
-                                        <div class="flex justify-center items-center col-span-4">
+                                    <div class="grid 2xl:grid-cols-10 grid-cols-2 w-full h-56 md:h-96">
+                                        <div class="flex 2xl:justify-center justify-end 2xl:col-span-4">
                                             <img src="{{ asset($doctor->image) }}" alt="doctor"
                                                 class="h-56 md:h-96 rounded-lg">
                                         </div>
-                                        <div class="pt-10 pr-20 text-white col-span-6">
+                                        <div class="pt-10 2xl:pr-20 px-5 pr-8 text-white 2xl:col-span-6">
                                             <h1 class="font-bold 2xl:text-6xl text-4xl mb-2">Dr. {{ $doctor->name }}
                                             </h1>
                                             <h1 class="font-semibold 2xl:text-4xl text-2xl 2xl:mb-8 mb-4">
@@ -100,41 +100,44 @@
             </div>
         </div>
         <div class="news">
-            <div class="grid grid-cols-2 gap-10 p-10 px-20">
+            <div class="grid grid-cols-2 gap-10 p-10 px-16 2xl:px-36">
                 <div>
-                    <img src="{{ asset('images/brain-tumor.jpg') }}"><br>
-                    <span class="text-white font-medium text-base">January 18, 2023</span><br>
-                    <span class="text-white font-bold text-3xl">Navigating the Spectrum of Spine Problems: A
-                        Comprehensive Medical Overview</span><br>
-                    <span class="text-white font-semibold text-lg">The spine, a remarkable structure supporting our body
-                        and facilitating movement, is
-                        susceptible to a
-                        variety of complex medical issues. From degenerative conditions to traumatic injuries,
-                        spine
-                        problems can significantly impact one's quality of life. In this blog post, we'll delve
-                        into the
-                        diverse world of spine problems, shedding light on different types, their underlying
-                        causes.</span>
+                    <img src="{{ asset('images/brain-tumor.jpg') }}" class="rounded-lg 2xl:h-auto 2xl:w-10/12">
+                    <p class="pt-3">
+                        <span class="text-white font-medium text-base">January 18, 2023</span><br>
+                        <span class="text-white font-bold text-3xl">Navigating the Spectrum of Spine Problems: A
+                            Comprehensive Medical Overview</span><br>
+                        <span class="text-white font-semibold text-lg">The spine, a remarkable structure supporting our
+                            body and facilitating movement, is susceptible to a
+                            variety of complex medical issues. From degenerative conditions to traumatic injuries, spine
+                            problems can significantly impact one's quality of life. In this blog post.</span>
+                    </p>
                 </div>
-                <div>
+                <div class="flex flex-col justify-between">
                     <div class="flex">
-                        <img src="{{ asset('images/headache-types.jpg') }}"class="h-48 w-74">
-                        <p class="pl-5 pt-5"><span class="text-white font-medium 2xl:text-base text-sm">January 18, 2023</span><br><br>
-                            <span class="text-white font-bold 2xl:text-2xl text-lg">Understanding Different Types of Headaches:
+                        <img src="{{ asset('images/headache-types.jpg') }}"class="h-48 w-74 rounded-lg">
+                        <p class="pl-5"><span class="text-white font-medium 2xl:text-base text-sm">January 18,
+                                2023</span><br>
+                            <span class="text-white font-bold 2xl:text-2xl text-lg">Understanding Different Types of
+                                Headaches:
                                 AMedical Overview</span>
                         </p>
                     </div>
                     <div class="flex">
-                        <img src="{{ asset('images/brain-tumor.jpg') }}" class="h-48 w-74">
-                        <p class="pl-5 pt-5"><span class="text-white font-medium 2xl:text-base text-sm">January 18, 2023</span><br><br>
-                            <span class="text-white font-bold 2xl:text-2xl text-lg">Navigating the Landscape of Brain Tumor Types: A
+                        <img src="{{ asset('images/brain-tumor.jpg') }}" class="h-48 w-74 rounded-lg">
+                        <p class="pl-5"><span class="text-white font-medium 2xl:text-base text-sm">January 18,
+                                2023</span><br>
+                            <span class="text-white font-bold 2xl:text-2xl text-lg">Navigating the Landscape of Brain
+                                Tumor Types: A
                                 Comprehensive Medical Guide</span>
                         </p>
                     </div>
                     <div class="flex">
-                        <img src="{{ asset('images/headache-types.jpg') }}" class="h-48 w-74">
-                        <p class="pl-5 pt-5"><span class="text-white font-medium 2xl:text-base text-sm">January 18, 2023</span><br><br>
-                            <span class="text-white font-bold 2xl:text-2xl text-lg">Navigating the Spectrum of Spine Problems: A
+                        <img src="{{ asset('images/headache-types.jpg') }}" class="h-48 w-74 rounded-lg">
+                        <p class="pl-5"><span class="text-white font-medium 2xl:text-base text-sm">January 18,
+                                2023</span><br>
+                            <span class="text-white font-bold 2xl:text-2xl text-lg">Navigating the Spectrum of Spine
+                                Problems: A
                                 Comprehensive Medical Overview</span>
                         </p>
                     </div>
